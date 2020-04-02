630865ba250a85b3c75843c5cbf6b1ed4ef1026a
ddl-salme <62372947+ddl-salme@users.noreply.github.com>
2020-04-02 00:32:21 -0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -4,14 +4,14 @@
 #   learn more at http://support.dominodatalab.com/hc/en-us/articles/209150326
  
 ## Python/Flask example
-#export LC_ALL=C.UTF-8
-#export LANG=C.UTF-8
-#export FLASK_APP=app-flask.py
-#export FLASK_DEBUG=1
-#python -m flask run --host=0.0.0.0 --port=8888
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+export FLASK_APP=app-flask.py
+export FLASK_DEBUG=1
+python -m flask run --host=0.0.0.0 --port=8888
  
 ## Dash example
 #python app-dash.py
  
 ## R/Shiny example
-R -e 'shiny::runApp("./", port=8888, host="0.0.0.0")'
+# R -e 'shiny::runApp("./", port=8888, host="0.0.0.0")'
